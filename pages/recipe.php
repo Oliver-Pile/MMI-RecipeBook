52169fa722850e33e7418cedd514e71253f03a29
Oliver Pile <oliver.pile@bt.com>
2023-11-02 11:52:18 +0000

    Store recipe images in DB

--- pages/recipe.php
+++ pages/recipe.php
@@ -1,58 +1,32 @@
-  <div class="container">
-    <h1 class="mb-4 pb-2">Crunchy Salad</h1>
-    <div class="row">
-      <div class="col-md-6">
+<?php
+  $Recipe = new Recipe($Conn);
+  $recipe_data = $Recipe->getRecipe($_GET['id']);
+?>
+<div class="container">
+  <h1 class="mb-4 pb-2"><?php echo $recipe_data['recipe_name']; ?></h1>
+  <div class="row">
+    <div class="col-md-6">
+      <?php if($recipe_data['images']) { ?>
         <div class="row">
+          <?php foreach($recipe_data['images'] as $image) { ?>
           <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/bbq.jpeg');">
-              <a href="./recipe-images/bbq.jpeg" data-lightbox="recipe-imgs"></a>
-              </div>
+          <div class="recipe-image mb-4" style="background-image: url('./recipe-images/<?php echo $image['recipe_image']; ?>');">
+          <a href="./recipe-images/<?php echo $image['recipe_image']; ?>" data-lightbox="recipe-imgs"></></a>
+            </div>
           </div>
-          <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/curry.jpg');">
-              <a href="./recipe-images/curry.jpg" data-lightbox="recipe-imgs"></a>
-              </div>
-          </div>
-          <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/chicken.jpeg');">
-              <a href="./recipe-images/chicken.jpeg" data-lightbox="recipe-imgs"></a>
-              </div>
-          </div>
-          <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/fruit.jpeg');">
-              <a href="./recipe-images/fruit.jpeg" data-lightbox="recipe-imgs"></a>
-              </div>
-          </div>
-          <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/pasta.jpeg');">
-              <a href="./recipe-images/pasta.jpeg" data-lightbox="recipe-imgs"></a>
-              </div>
-          </div>
-          <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/pizza.jpeg');">
-              <a href="./recipe-images/pizza.jpeg" data-lightbox="recipe-imgs"></a>
-              </div>
-          </div>
-          <div class="col-md-4">
-            <div class="recipe-image mb-4" style="background-image: url('./recipe-images/salmon.jpeg');">
-              <a href="./recipe-images/salmon.jpeg" data-lightbox="recipe-imgs"></a>
-              </div>
-          </div>
+          <?php } ?>
         </div>
-      </div>
-      <div class="col-md-6">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dui felis, gravida nec arcu a, interdum fermentum est. Phasellus faucibus rutrum maximus. Nam a nibh quis leo egestas ultricies ut eget orci. Ut consequat augue ac leo vestibulum venenatis. Nullam hendrerit varius urna at faucibus. Quisque rutrum erat dui, a euismod velit maximus in. Donec suscipit sapien vitae nisi commodo, et dapibus nisl interdum. Maecenas pulvinar velit a felis malesuada, eget pharetra mi venenatis. Duis erat ipsum, laoreet sed laoreet tempor, tempor sit amet lacus. Nullam auctor libero risus, et dignissim ex tincidunt vitae.
-        </p>
-        <p>
-          Integer ornare varius varius. Aliquam erat volutpat. Nunc quis efficitur elit. Donec eget pretium leo. Maecenas fringilla lacus in elit faucibus, vel ultricies sapien pulvinar. Morbi consectetur magna sed auctor tincidunt. Nulla pharetra dictum metus eget luctus. Cras in malesuada mauris. Sed a dictum tellus, ac pulvinar mauris. Morbi at massa dui. Phasellus accumsan risus non mauris ultricies dapibus.
-        </p>
-        <ul class="recipe-features">
-          <li><i class="far fa-clock"></i> 20 Min</li>
-          <li><i class="fas fa-users"></i> 4 Servings</li>
-          <li><i class="fas fa-dollar-sign"></i> Budget</li>
-          <li><i class="fas fa-tags"></i> salad, healthy, vegetarian</li>
-        </ul>
-      </div>
-      </div>
+      <?php } ?>
+    </div>
+
+    <div class="col-md-6">
+      <p><?php echo $recipe_data['recipe_instructions']; ?></p>
+      <ul class="recipe-features">
+      <li><i class="far fa-clock"></i> <?php echo $recipe_data['recipe_time']; ?></li>
+      <li><i class="fas fa-users"></i> <?php echo $recipe_data['recipe_servings']; ?> Servings</li>
+      <li><i class="fas fa-dollar-sign"></i> <?php echo $recipe_data['recipe_price']; ?></li>
+      <li><i class="fas fa-tags"></i> <?php echo $recipe_data['recipe_tags']; ?></li>
+      </ul>
+    </div>
   </div>
+</div>
