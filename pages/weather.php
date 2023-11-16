7cf4aec9e54b10edeaf320a5695b46e3d9f64468
Oliver Pile <oliver.pile@bt.com>
2023-11-16 10:24:48 +0000

    Add weather API

---
+++ pages/weather.php
@@ -0,0 +1,15 @@
+<?php
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+  $dotenv->load();
+
+  $ch = curl_init();
+  $location = 'Ipswich,UK';
+  $apikey = $_ENV['WEATHER_API_KEY'];
+
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_URL, 'http://api.openweathermap.org/data/2.5/weather?q='.$location.'&appid='.$apikey);
+  $output = curl_exec($ch);
+  curl_close($ch);
+
+  echo "<p>The weather in ".$location." is ".$output['weather'][0]['main']."</p>";
+?>
